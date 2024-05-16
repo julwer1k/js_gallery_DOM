e6911ec26b293b03a48501f82fefda731a8279cf
ArtyWhit <julwer1k@gmail.com>
2024-05-16 19:21:27 +0300

    make build

---
+++ index.90d4162c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault(),"IMG"===t.target.tagName?e.src=t.target.parentElement.href:"A"===t.target.tagName&&(e.src=t.target.href)});
+//# sourceMappingURL=index.90d4162c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const mainImage = document.querySelector("#largeImg");
+const thumbs = document.getElementById("thumbs");
+function changeImage(e) {
+    e.preventDefault();
+    if (e.target.tagName === "IMG") mainImage.src = e.target.parentElement.href;
+    else if (e.target.tagName === "A") mainImage.src = e.target.href;
+}
+thumbs.addEventListener("click", changeImage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
